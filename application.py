94a95adf984939efe12a05d067226b9ceadc6c25
AnujMittal07 <am431@snu.edu.in>
2024-06-15 02:24:42 +0530

    Made a comment

--- application.py
+++ application.py
@@ -10,7 +10,7 @@ app = application
 # Import pickle files
 ridge_model = pickle.load(open('models/ridge.pkl','rb'))
 scaler_model = pickle.load(open('models/scaler.pkl','rb'))
-
+# Checking fot one change made
 
 @app.route('/')
 def index():
